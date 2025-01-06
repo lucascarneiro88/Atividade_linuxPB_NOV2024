a34dc280d26dab65dc77ca21d90e46df09bb9078
Lucas <carneirolucas88@gmail.com>
2025-01-06 16:08:59 -0300

    feat: aprimora verificação de status do serviço Nginx
    Aprimora a verificação do status do serviço Nginx, tornando a execução mais eficiente e robusta.

--- valida_nginx.sh
+++ valida_nginx.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # data e hora atual
@@ -17,14 +16,21 @@ mkdir -p $DIRETORIO_LOG
 SYSTEMCTL="/bin/systemctl"  # ou /usr/bin/systemctl
 
 # verifica o status do servico usando systemctl
-STATUS=$($SYSTEMCTL status $SERVICO | grep "Active:")
+STATUS=$($SYSTEMCTL status $SERVICO)
 
 # verifica se o servico esta ativo ou não
-if echo "$STATUS" | grep -q "active"; then
+if echo "$STATUS" | grep -q "active (running)"; then
     # se o servico estiver online, registra o status no arquivo online.log e exibe mensagem
     echo "$DATA_HORA - Serviço $SERVICO está ONLINE" >> $DIRETORIO_LOG/online.log
-else
-    # se o servico estiver offline, registra o status no arquivo offline.log e exibe mensagem
+elif echo "$STATUS" | grep -q "inactive (dead)"; then
+    # se o servico estiver inativo, registra o status no arquivo offline.log e exibe mensagem
     echo "$DATA_HORA - Serviço $SERVICO está OFFLINE" >> $DIRETORIO_LOG/offline.log
+elif echo "$STATUS" | grep -q "failed"; then
+    # se o servico falhou, registra o status no arquivo offline.log
+    echo "$DATA_HORA - Serviço $SERVICO está FALHADO" >> $DIRETORIO_LOG/offline.log
+else
+    # caso o status seja outro, registra como offline
+    echo "$DATA_HORA - Serviço $SERVICO está COM PROBLEMAS TENTE MAIS TARDE (status desconhecido)" >> $DIRETORIO_LOG/offline.log
 fi
+
 
